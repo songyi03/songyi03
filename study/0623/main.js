4195a7f73f12050efd33f1c690fef548d649f3db
Your Name <you@example.com>
2025-06-23 12:52:33 +0900

    오전수업.commit

---
+++ study/0623/main.js
@@ -0,0 +1,14 @@
+// alert("Hello World");
+// console.log("Hello Wolrd");
+// document.write("<h1>Document Write</h1>");
+// let currentYear = 2025;
+// let birthYear = prompt ("당신의 태어난 해를 입력하세요");
+// let age = currentYear - birthYear + 1;
+// alert("당신의 나이는 " + age + "살 입니다.");
+let name = "철수";
+let age = 20;
+let isStudent = true;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
