f3e1370c902033decb3cadf2c67053da72e91db7
Your Name <you@example.com>
2025-07-10 16:44:50 +0900

    0710

---
+++ study/0710/Ch03_2/src/components/Icon.tsx
@@ -0,0 +1,10 @@
+import type {FC, CSSProperties} from 'react'
+
+export type IconProps = {
+    name: string
+    style?: CSSProperties
+}
+
+export const Icon: FC<IconProps> =({name, style}) => {
+    return <span className="material-icons" style={style}>{name}</span>
+}
