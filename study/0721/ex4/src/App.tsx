6802d0a2f74ae5e1b4be6bd74dc4afac2623e9fc
john <you@example.com>
2025-07-21 16:39:23 +0900

    오후수업

--- study/0721/ex4/src/App.tsx
+++ study/0721/ex4/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MemoTest from './pages/MemoTest'
-
+// import MemoTest from './pages/MemoTest';
+import HookTest from './pages/HookTest';
 
 function App() {
   return (
     <div className="App">
-    <MemoTest />
+    {/* <MemoTest/> */}
+    <HookTest/>
     </div>
   );
 }
