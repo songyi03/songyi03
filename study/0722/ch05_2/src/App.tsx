78bc001b726975d34ab1f473f340f098d4b07f21
john <you@example.com>
2025-07-22 16:27:15 +0900

    0722수업

--- study/0722/ch05_2/src/App.tsx
+++ study/0722/ch05_2/src/App.tsx
@@ -3,14 +3,14 @@ import {useStore} from './store'
 import ClockTest from './pages/ClockTest'
 import CounterTest from './pages/CounterTest'
 import RemoteUserTest from './pages/RemoteUserTest'
-import CardsTest from './pages/CardsTest'
+
 
 export default function App() {
   const store = useStore()
   return (
     <ReduxProvider store={store}>
       {/* 리액트 버전 18 대응 추가 요소 (ReduxProvider에 반드시 1개 이상의 자식 요소를 가져야함) */}
-      <CardsTest />
+    
       <RemoteUserTest />
       <CounterTest />
       <ClockTest />
